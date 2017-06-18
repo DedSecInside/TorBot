a2f21ad5618e8801bc0c672a1edbab2c3b92713c
agrepravin <agrepravin@gmail.com>
2017-06-18 20:08:56 +0530

    Update test.py
    changed connect parameter

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 from stem.connection import connect
 
 if __name__ == '__main__':
-  controller = connect("agrepravin")
+  controller = connect("Narayanan123")
 
   if not controller:
     sys.exit(1)  # unable to get a connection
