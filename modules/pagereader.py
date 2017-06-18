60ed27eaa5177ba611fcba3ec63d484b59eca264
PSN <psn@localhost.localdomain>
2017-06-18 23:06:08 +0530

    Tor Connection and Page Parsing

--- modules/pagereader.py
+++ modules/pagereader.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 
 
 def readPage(site):
-    response = urllib.request.urlopen(site)
-    page = BeautifulSoup(response.read(),'html.parser')
-    print (page.find_all('input'))
-    return page
+
+ headers = {'User-Agent': 'JAMES CAMPBELL jamescampbell.us SEARCH BOT! I FOUND YOU!!!!' }
+ req = urllib.request.Request(site,None,headers)
+ response = urllib.request.urlopen(req)
+ page = BeautifulSoup(response.read(),'html.parser')
+ print (page.find_all('strong'))
+ return page
--- torBot.py
+++ torBot.py
@@ -1,6 +1,22 @@
 from modules.getemails import getMails
 from modules.getweblinks import getLinks
 from modules.pagereader import readPage
+import socket
+import socks
+import argparse
+import random
+import sys
+
+
+#TOR SETUP GLOBAL Vars
+SOCKS_PORT = 9050  # TOR proxy port that is default from torrc, change to whatever torrc is configured to
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1",SOCKS_PORT)
+socket.socket = socks.socksocket
+# Perform DNS resolution through the socket
+def getaddrinfo(*args):
+  return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+  
+socket.getaddrinfo = getaddrinfo
 
 class bcolors:
     HEADER = '\033[95m'
@@ -63,27 +79,17 @@ def header():
 	print("######################################################")
 	print(bcolors.FAIL + "LICENSE: GNU Public License" + bcolors.ENDC)
 	print()
-
-def stemTest():
- from stem.control import Controller
- with Controller.from_port(port = 9051) as controller:
-  controller.authenticate("Narayanan123")
-  print (controller)
-  bytes_read = controller.get_info("traffic/read")
-  bytes_written = controller.get_info("traffic/written")
-  print("My Tor relay has read %s bytes and written %s." % (bytes_read, bytes_written))
+   
 
 def main():
  header()
- stemTest()
  print ("Tor Ip Address :")
- a = readPage("http://www.whatsmyip.net/")
- #a = readPage("http://andris23zrbn2da5.onion")
- print (type(a))
- getMails(a)
- getLinks(a)
+ a = readPage("https://check.torproject.org/")
+ print ("Onion Site Test")
+ b = readPage("http://torlinkbgs6aabns.onion/")
+ getMails(b)
+ getLinks(b)
  print ("\n\n")
- #readPage("http://od6j46sy5zg7aqze.onion")
  return 0
 
 if __name__ == '__main__':
