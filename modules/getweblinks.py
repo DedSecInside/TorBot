0e2fc2f2dd45904238ecf61d2e8c006c96dfba82
PSNAppZ <thepsnarayanan@gmail.com>
2017-06-29 13:37:46 +0530

    Major Update 1.0.0

--- modules/getweblinks.py
+++ modules/getweblinks.py
@@ -6,18 +6,24 @@ from modules.bcolors import bcolors
 import bs4
 
 """Get all onion links from the website"""
-def getLinks(soup):
+def getLinks(soup,ext):
     _soup_instance = bs4.BeautifulSoup
-    extensions = ['.onion','.onion/']
+    extensions = []
+    if ext:
+        for e in ext:
+            extensions.append(e)  
     if isinstance(type(soup), type(_soup_instance)):
         websites = []
         for link in soup.find_all('a'):
             web_link = link.get('href')
             if web_link != None:
-                if 'http' in web_link:
-                    for extension in extensions:
-                        if web_link.endswith(extension):
-                            websites.append(web_link)
+                if ('http' in web_link or 'https' in web_link):
+                    if ext:
+                        for exten in extensions:
+                            if web_link.endswith(exten):
+                                websites.append(web_link)
+                    else:
+                        websites.append(web_link)            
             else:
                 pass
         """Pretty print output as below"""
