56c45ae1d06a3f1cfa9d87948bd00f2ea68435f1
Manfredi <manfredimartorana@gmail.com>
2017-12-23 19:03:20 +0000

    fixed the error occurring when given a broken link with -u flag

--- modules/pagereader.py
+++ modules/pagereader.py
@@ -8,10 +8,21 @@ def readPage(site,printIP=0):
 
  headers = {'User-Agent': 'TorBot - Onion crawler | www.github.com/DedSecInside/TorBot' }
  req = urllib.request.Request(site,None,headers)
- response = urllib.request.urlopen(req)
- page = BeautifulSoup(response.read(),'html.parser')
- if printIP:
-  pg = page.find('strong')
-  IP = pg.renderContents()
-  print(Bcolors.WARNING+Bcolors.BOLD+IP.decode("utf-8")+Bcolors.ENDC)
+
+ try:
+  response = urllib.request.urlopen(req)
+  page = BeautifulSoup(response.read(), 'html.parser')
+
+  if printIP:
+   pg = page.find('strong')
+   IP = pg.renderContents()
+   print(Bcolors.WARNING + Bcolors.BOLD + IP.decode("utf-8") + Bcolors.ENDC)
+ except urllib.error.URLError:
+  pass
+
  return page
+
+
+
+
+
