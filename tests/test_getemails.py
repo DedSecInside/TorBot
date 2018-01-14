fe8b0a59fc0a5627590c2c958094aa667d8ec4fe
Akeem King <akeemtlking@gmail.com>
2018-01-14 01:27:36 -0500

    Changed testing framework from unittest to pytest
    Code is much simpler, cleaner and more pythonic.
    Also can run test suite much easyer now by just using
    pytest

--- tests/test_getemails.py
+++ tests/test_getemails.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import unittest
 
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(
@@ -9,22 +8,12 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from modules import pagereader, getemails
-from modules.bcolors import Bcolors
 
 
-soup = pagereader.readPage('http://www.whatsmyip.net/')
+def test_get_emails_successful():
+    soup = pagereader.readPage('http://www.whatsmyip.net/')
+    test_emails = ["advertise@provaz.eu"]
+    emails = getemails.getMails(soup)
+    assert emails == test_emails
 
 
-class getMailsTestCase(unittest.TestCase):
-
-        def setUp(self):
-            self.b_colors = Bcolors()
-
-        def test_getemails(self):
-            test_emails = ["advertise@provaz.eu"]
-            emails = getemails.getMails(soup)
-            self.assertEqual(emails, test_emails)
-
-
-if __name__ == '__main__':
-    unittest.main()
--- tests/test_getweblinks.py
+++ tests/test_getweblinks.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import unittest
 import sys
 import os
 PACKAGE_PARENT = '..'
@@ -8,30 +7,15 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(
              os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-from io import StringIO
-from modules.bcolors import Bcolors
 from modules import getweblinks, pagereader
 
-soup = pagereader.readPage('http://www.whatsmyip.net/')
 
+def test_get_links_successful():
+    soup = pagereader.readPage('http://www.whatsmyip.net/')
+    data = ['http://aff.ironsocket.com/SH7L',
+            'http://aff.ironsocket.com/SH7L',
+            'http://wsrs.net/',
+            'http://cmsgear.com/']
 
-class getLinksTestCase(unittest.TestCase):
-
-        def setUp(self):
-            self.b_colors = Bcolors()
-            self.held, sys.stdout = sys.stdout, StringIO()
-            self.maxDiff = None
-
-        def test_get_links(self):
-
-            data = ['http://aff.ironsocket.com/SH7L',
-                    'http://aff.ironsocket.com/SH7L',
-                    'http://wsrs.net/',
-                    'http://cmsgear.com/']
-
-            result = getweblinks.getLinks(soup)
-            self.assertEqual(result, data)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    result = getweblinks.getLinks(soup)
+    assert result == data
