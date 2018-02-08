d2f9ef8311fe998846878bea98ebdf230a41ad59
Akeem King <akeemtlking@gmail.com>
2018-02-08 08:14:02 -0500

    Fixed `-l` flag for finding live links.
    Takes list of URLs and asychrnously calls HEAD request on each
    link in the list and test status code for a 200 response. If the
    response is not 200 or takes longer than 8 seconds, the link is
    declared dead. Also switched from urllib.request to requests for
    not only simplicity but thread-safety also.

--- modules/getweblinks.py
+++ modules/getweblinks.py
@@ -1,52 +1,61 @@
 import re
-from modules.bcolors import Bcolors
+import requests
 from bs4 import BeautifulSoup
+from modules.bcolors import Bcolors
 
 
-def valid_onion_url(link):
+def valid_url(url):
+    """Checks if url is valid using regular expression matching
 
-    """
-        Validates onion urls using regex
+        Matches all possible url patterns with the url that is passed and returns
+        True if it is a url and returns False if it is not.
 
         Args:
-            link: the url to be checked
+            url: string representing url to be checked
 
         Returns:
-            bool: True/False based on link
+            bool: True if valid url format, False if not
     """
+    regex = re.compile(
+            r'^(?:http|ftp)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    pattern = r"^https?\b(://+)(.+)(.+)\bonion/(.*)"
-    re_obj = re.compile(pattern)
-    if re_obj.fullmatch(link):
+    if regex.match(url):
         return True
 
     return False
 
 
-def valid_url(link):
+def get_link_status(link, colors):
+    """Generator that yields links as they come
 
-    """
-        Validates general urls using regex
-
-        Takes in string which is a link and returns decides validitity of url
-        using regex
+        Uses head request because it uses less bandwith than get and timeout is
+        set to 10 seconds and then link is automatically declared as dead.
 
         Args:
-            link: the url to be checked
+            link: link to be tested
+            colors: object containing colors for link
 
-        Returns:
-            bool: True/False based on link
+        Yields:
+            string: link with either no color or red which indicates failure
     """
 
-    pattern = r"^https?\b(://+)(.+)(.+)\b...(.*)"
-    re_obj = re.compile(pattern)
-    if re_obj.fullmatch(link):
-        return True
+    try:
+        resp = requests.head(link, timeout=10)
+    except requests.exceptions.ConnectionError:
+        yield '\t'+colors.On_Red+link+colors.ENDC
 
-    return False
+    if not resp.status_code == 200:
+        yield '\t'+colors.On_Red+link+colors.ENDC
+    else:
+        yield '\t'+link
 
 
-def getLinks(soup):
+def getLinks(soup, ext=False, live=False):
 
     """
         Searches through all <a ref> (hyperlinks) tags and stores them in a
@@ -56,25 +65,29 @@ def getLinks(soup):
             soup: BeautifulSoup instance currently being used.
 
         Returns:
-            websites: List of websites that were found
+            wfebsites: List of websites that were found
     """
-
     b_colors = Bcolors()
-
-    if isinstance(type(soup), type(BeautifulSoup)):
+    if isinstance(soup, BeautifulSoup):
         websites = []
-
         links = soup.find_all('a')
         for ref in links:
             url = ref.get('href')
-            if url and (valid_onion_url(url) or valid_url(url)):
+            if url and valid_url(url):
+                if ext:
+                    for extension in ext:
+                        if not url.endswith(extension):
+                            continue
                 websites.append(url)
 
         """Pretty print output as below"""
         print(''.join((b_colors.OKGREEN,
               'Websites Found - ', b_colors.ENDC, str(len(websites)))))
-        print('-------------------------------')
-        return websites
+        print('------------------------------------')
+        if live:
+            for link in websites:
+                print(next(get_link_status(link, b_colors)))
+            return websites
 
     else:
         raise('Method parameter is not of instance BeautifulSoup')
