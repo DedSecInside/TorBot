00d47448ee10d0d720c7b506fbd832ec54a53995
Akeem King <akeemtlking@gmail.com>
2018-02-18 13:53:08 -0500

    Added modules folder

---
+++ modules/bcolors.py
@@ -0,0 +1,14 @@
+class Bcolors:
+
+    def __init__(self):
+        self.HEADER = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.UNDERLINE = '\033[4m'
+        self.WHITE = '\033[97m'
+        self.On_Black = '\033[40m'
+        self.On_Red = '\033[41m'
---
+++ modules/pagereader.py
@@ -0,0 +1,65 @@
+import requests
+
+from bs4 import BeautifulSoup
+from modules.bcolors import Bcolors
+from requests.exceptions import ConnectionError, HTTPError
+from sys import exit
+
+
+def connection_msg(site):
+    yield "Attempting to connect to {site}".format(site=site)
+
+
+def readPage(site, extension=False):
+    headers = {'User-Agent':
+               'TorBot - Onion crawler | www.github.com/DedSecInside/TorBot'}
+    attempts_left = 3
+    err = " "
+    while attempts_left:
+        try:
+            if not extension:
+                print(next(connection_msg(site)))
+                response = requests.get(site, headers=headers)
+                print("Connection successful.")
+                page = BeautifulSoup(response.text, 'html.parser')
+                return page
+            if extension and attempts_left == 3:
+                print(next(connection_msg('https://'+site)))
+                response = requests.get('https://'+site, headers=headers)
+                print("Connection successful.")
+                page = BeautifulSoup(response.text, 'html.parser')
+                return page
+            if extension and attempts_left == 2:
+                print(next(connection_msg('http://'+site)))
+                response = requests.get('http://'+site, headers=headers)
+                print("Connection successful.")
+                page = BeautifulSoup(response.text, 'html.parser')
+                return page
+            if extension and attempts_left == 1:
+                msg = ''.join(("There has been an {err} while attempting to ",
+                              "connect to {site}.")).format(err=err, site=site)
+                exit(msg)
+
+        except (HTTPError, ConnectionError) as e:
+            attempts_left -= 1
+            err = e
+
+    if err == HTTPError:
+        raise("There has been an HTTP error after three attempts.")
+    if err == ConnectionError:
+        raise("There has been a connection error after three attempts.")
+
+
+def get_ip():
+    """Returns users tor ip address
+
+    https://check.torproject.org/ tells you if you are using tor and it
+    displays your IP address which we scape and return
+    """
+
+    b_colors = Bcolors()
+    page = readPage('https://check.torproject.org/')
+    pg = page.find('strong')
+    ip_addr = pg.renderContents()
+
+    return b_colors.WARNING+b_colors.BOLD+ip_addr.decode("utf-8")+b_colors.ENDC
