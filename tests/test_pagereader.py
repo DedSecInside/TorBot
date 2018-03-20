96301e4ad5dd25a54f6033908cde1dfdf2cd1343
Akeem King <akeemtlking@gmail.com>
2018-03-19 21:02:49 -0400

    Fixing typo

--- tests/test_pagereader.py
+++ tests/test_pagereader.py
@@ -5,6 +5,7 @@ import requests_mock
 from bs4 import BeautifulSoup
 from requests.exceptions import HTTPError, ConnectionError
 
+
 @pytest.fixture
 def test_read_first_page(site, extension=False):
 
@@ -16,12 +17,12 @@ def test_read_first_page(site, extension=False):
 
         m.register_uri('GET', 'https://www.test.cannotbefound', exc=HTTPError)
         m.register_uri('GET', 'http://www.test.cannotbefound', exc=HTTPError)
-        
+
         headers = {'User-Agent':
                    'TorBot - Onion crawler | www.github.com/DedSecInside/TorBot'}
         attempts_left = 3
         err = " "
-        
+
         # Removed unnecessary code such as printing
         while attempts_left:
             try:
@@ -46,23 +47,28 @@ def test_read_first_page(site, extension=False):
 
         raise err
 
+
 def test_single_extension():
     uris = {
-        '.com':'www.test.com',
-        '.org':'www.test.org',
-        '.net':'www.test.net',
-        '.onion':'www.test.onion',
+        '.com': 'www.test.com',
+        '.org': 'www.test.org',
+        '.net': 'www.test.net',
+        '.onion': 'www.test.onion',
         '.cannotbefound': 'www.test.cannotbefound'
             }
-    
+
     with pytest.raises(HTTPError):
         for toplevel_domain, url in uris.items():
-           page = test_read_first_page(url, toplevel_domain) 
-           if toplevel_domain == '.com':
-               assert page == 'This is a dot com site.'
-           elif toplevel_domain == '.org':
-               assert page == 'This is a dot org site.'
-           elif toplevel_domain == '.net':
-               assert page == 'This is a dot net site.'
-           elif topleveldomain == '.onion':
-               assert page == 'This is a dot onion site.'
+            page = test_read_first_page(url, toplevel_domain)
+            if toplevel_domain == '.com':
+                assert page == 'This is a dot com site.'
+            elif toplevel_domain == '.org':
+                assert page == 'This is a dot org site.'
+            elif toplevel_domain == '.net':
+                assert page == 'This is a dot net site.'
+            elif toplevel_domain == '.onion':
+                assert page == 'This is a dot onion site.'
+
+
+if __name__ == '__main__':
+    test_single_extension()
