e76a578fae51f0dc455479c37f870f1628d5ad68
Tiago Matias <tiago.matias92@gmail.com>
2018-03-22 14:47:57 +0000

    Added Database Register

---
+++ modules/savedb.py
@@ -0,0 +1,67 @@
+import time
+import json
+import MySQLdb
+import sys
+import argparse
+
+def saveToDatabase(database, user, password, links):
+        """
+        Connects to a MYSQL DB
+        Create MYSQL Table
+        Add link to the DB
+
+        Args:
+                data = data that is being stored in the database
+                user = username to login into MYSQL
+                password = password of MYSQL
+                link = URLs from the crawler
+        """
+        if database and user and password is None:
+                print("Wrong DB Credentials")
+                exit()
+
+        #Debug
+        #print("Database:", database, "\nuser:",user, "\npass:",password)
+        db = MySQLdb.connect(host="localhost",    # your host
+                                user=user,         # your username
+                                passwd=password,  # your password
+                                db=database)        # name of the data base
+
+        cur = db.cursor()
+
+        try:
+                query = """ CREATE TABLE IF NOT EXISTS `tor_url` (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                link VARCHAR(30) NOT NULL UNIQUE,
+                reg_date TIMESTAMP)"""
+                cur.execute(query)
+                first_result = cur.fetchall()
+                print(first_result)
+                for link in links:
+                        query = "INSERT IGNORE INTO `tor_url` (link) VALUES ('{0}')".format(link)
+                        cur.execute(query)
+                        print(query)
+                
+                second_result = cur.fetchall()
+                print(second_result)
+                db.commit()
+                
+        except (MySQLdb.Error, MySQLdb.Warning) as e:
+                print(e)
+                return None
+
+        except TypeError as e:
+                print(e)
+                return None
+
+        except ValueError as e:
+                print(e)
+                return None
+
+        finally:
+                cur.close()
+                db.close()
+
+
+
+    
--- torBot.py
+++ torBot.py
@@ -1,8 +1,10 @@
 import argparse
 import socket
 import socks
+import os
+import settings
 from modules import (bcolors, getemails, pagereader, getweblinks, updater,
-                     info, savefile)
+                     info, savefile, savedb)
 
 
 LOCALHOST = "127.0.0.1"
@@ -138,6 +140,9 @@ def main(conn=False):
                         action="store_true",
                         help=' '.join(("Info displays basic info of the",
                                        "scanned site, (very slow)")))
+    parser.add_argument("-db", "--database",
+                            action="store_true",
+                            help="Specify a database to connect.")                                    
     args = parser.parse_args()
 
     link = args.url
@@ -150,6 +155,16 @@ def main(conn=False):
     if args.update:
         updater.updateTor()
         exit()
+    if args.database:
+        if 'DATABASE_NAME' and 'DATABASE_USERNAME' and 'DATABASE_PASSWORD' in os.environ:
+            DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+            DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+            DATABASE_NAME = os.getenv("DATABASE_NAME")
+            print("DB - ", DATABASE_NAME, " - ", DATABASE_USERNAME, " - ", DATABASE_PASSWORD)
+        else:
+            print("Wrong Database Configurations")
+            exit()
+
     if not args.quiet:
         header()
     # If url flag is set then check for accompanying flag set. Only one
@@ -174,8 +189,10 @@ def main(conn=False):
                                           ext=args.extension)
             if args.save:
                 savefile.saveJson("Links", links)
+            if(args.database):
+                savedb.saveToDatabase(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, links)
     else:
-        print("usage: torBot.py [-h] [-v] [--update] [-q] [-u URL] [-s] [-m] [-e EXTENSION] [-l] [-i]")
+        print("usage: torBot.py [-h] [-v] [--update] [-q] [-u URL] [-s] [-m] [-e EXTENSION] [-l] [-i] [-db]")
 
     print("\n\n")
 
