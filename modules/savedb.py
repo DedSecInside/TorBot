b723ba3adcf0267c4da76038cf0317405714ae21
Tiago Matias <tiago.matias92@gmail.com>
2018-04-01 22:53:08 +0000

    Requested changes applied #75

--- modules/savedb.py
+++ modules/savedb.py
@@ -1,7 +1,5 @@
-import time
-import json
 import MySQLdb
-import sys
+
 
 def saveToDatabase(database, user, password, links):
         """
@@ -22,29 +20,32 @@ def saveToDatabase(database, user, password, links):
         #Debug
         #print("Database:", database, "\nuser:",user, "\npass:",password)
         try:
-            db = MySQLdb.connect(host="localhost1",    # your host
+            db = MySQLdb.connect(host="localhost",    # your host
                                     user=user,         # your username
                                     passwd=password,  # your password
                                     db=database)        # name of the data base
 
             cur = db.cursor()
-            try:
-                    query = """ CREATE TABLE IF NOT EXISTS `tor_url` (
-                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                    link VARCHAR(30) NOT NULL UNIQUE,
-                    reg_date TIMESTAMP)"""
-                    cur.execute(query)
-                    for link in links:
-                            query = "INSERT IGNORE INTO `tor_url` (link) VALUES ('{0}')".format(link)
-                            cur.execute(query)
-                            #print(query)
-                    db.commit()
 
-            except (MySQLdb.Error, MySQLdb.Warning, TypeError, ValueError) as e:
-                    print(e)
-                    return None
-            finally:
-                    cur.close()
-                    db.close()
-        except:
+        except :
             print("Unable to connect to database")
+
+        try:
+                query = """ CREATE TABLE IF NOT EXISTS `tor_url` (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                link VARCHAR(30) NOT NULL UNIQUE,
+                reg_date TIMESTAMP)"""
+                cur.execute(query)
+                for link in links:
+                        query = "INSERT IGNORE INTO `tor_url` (link) VALUES ('{0}')".format(link)
+                        cur.execute(query)
+                        #print(query)
+                db.commit()
+
+        except (MySQLdb.Error, MySQLdb.Warning, TypeError, ValueError) as e:
+                print(e)
+                return None
+        finally:
+                cur.close()
+                db.close()
+
