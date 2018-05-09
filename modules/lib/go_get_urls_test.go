376f45ffac84f670e3e711856650d55d26773fb3
Akeem Tyrell Lamont King <akeemking@Machine.local>
2018-05-08 20:26:27 -0400

    Adding more coverage for tests

--- modules/lib/go_get_urls_test.go
+++ modules/lib/go_get_urls_test.go
@@ -1,6 +1,13 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"testing"
+
+	"github.com/mgutz/ansi"
+)
 
 func TestValidOnionURL(t *testing.T) {
 	table := []struct {
@@ -22,6 +29,46 @@ func TestValidOnionURL(t *testing.T) {
 			t.Errorf("%v received the value %v for being a valid onion url.",
 				testData.url,
 				validOnionURL(testData.url))
+		}
+	}
+}
+
+type mockClient struct {
+	mockResponse map[string]*http.Response
+	Error        error
+}
+
+func (client mockClient) Head(url string) (*http.Response, error) {
+	return client.mockResponse[url], client.Error
+}
+
+func TestCheckURL(t *testing.T) {
+	ch := make(chan string)
+	red := ansi.ColorFunc("red")
+	var err error
+
+	for i := 0; i < 10; i++ {
+		stCode := rand.Intn(600)
+		if stCode > 400 {
+			err = http.ErrNoLocation
+		} else {
+			err = nil
+		}
+		client := mockClient{map[string]*http.Response{"url": &http.Response{StatusCode: stCode}}, err}
+		go checkURL(client, "url", ch)
+		res := <-ch
+
+		switch {
+		case stCode <= 400:
+			desiredRes := fmt.Sprint("url is reachable.\n")
+			if res != desiredRes {
+				t.Errorf("Status Code of %v returned %v", stCode, res)
+			}
+		default:
+			desiredRes := red(fmt.Sprint("url is not reachable.\n"))
+			if res != desiredRes {
+				t.Errorf("Status Code of %v returned %v", stCode, res)
+			}
 		}
 	}
 }
