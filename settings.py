ca41fdeb3da148fb09063892013c66182eb1fcdb
shivankarmadaan <shivankar.madaan@sprinklr.com>
2018-08-22 17:45:49 +0530

    mysql to mongo

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv	
-from pathlib import Path  # python3 only	
-env_path = Path('.') / '.env'	
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
+env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
