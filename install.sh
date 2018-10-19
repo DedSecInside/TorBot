dadac88ef2b59863ef0cc3739c48472d498de2a1
Akeem King <akeemtlking@gmail.com>
2018-10-19 03:11:58 -0400

    Updating install script

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Get Golang Dependencies
-go get github.com/mgutz/ansi
-go get golang.org/x/net/html
-
 # Makes directory for dependencies and executable to be installed
 mkdir -p tmp_build 
 mkdir -p tmp_dist
