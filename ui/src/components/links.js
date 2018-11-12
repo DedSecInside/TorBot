540e68ed32eb32a01aba19abe47a2403ccf7639f
Akeem King <akeemtlking@gmail.com>
2018-11-12 17:34:05 -0500

    Adding home button

--- ui/src/components/links.js
+++ ui/src/components/links.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './home';
+import './links.css';
 
 class Links extends React.Component {
     constructor(props) {
         super(props);
         this.links = props.links;
+        this.onHome = this.onHome.bind(this);
+    }
+
+    onHome(event) {
+        ReactDOM.render(<Home/>, document.getElementById('root'));
     }
 
     render() {
         let items = this.links;
         return (
+            <React.Fragment>
             <ol>
                 {items.map((item, i) => <li key={i}>{item}</li>)}
             </ol>
+            <input type='button' onClick={this.onHome} value='HOME' className='home-button'/>
+            </React.Fragment>
         )
     }
 }
