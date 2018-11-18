43dd3595065c7e57fa6246779a93d6bfb02d47d1
Akeem King <akeemtlking@gmail.com>
2018-11-18 11:01:24 -0500

    Adding documentation

--- modules/proxy.py
+++ modules/proxy.py
@@ -1,22 +1,56 @@
+"""
+Module contains onion proxy related code
+"""
 import requests
 
 proxies = {
-        'http': 'socks5h://localhost:9050',
-        'https': 'socks5h://localhost:9050'
+    'http': 'socks5h://localhost:9050',
+    'https': 'socks5h://localhost:9050'
 }
 
 session = requests.session()
 session.proxies = proxies
 
+def setProxy(host, port):
+    """
+    Sets host and port for tor proxies being used when performing 
+    irequests to onion domains.
+
+    Args:
+        host (string): host name or IP Address
+        port (string): port number
+    """
+    global proxies # Allows us to modify global variable directly
+    global session
+    proxies = {
+        'http': 'socks5h://' + host + ':' + port,
+        'https': 'socks5h://' + host + ':' + port
+    }
+    session.proxies = proxies
+
 def proxyGET(url, headers=None):
+    """
+    Peforms GET request using socks5 proxy
+    
+    Args:
+        url (string): url to retreive 
+        headers (dict): mapping of headers for requests
+    """
     if headers:
         return session.get(url, headers=headers)
-    else:
-        return session.get(url)
+
+    return session.get(url)
+
 
 def proxyHEAD(url, timeout=10, headers=None):
+    """
+    Peforms HEAD request using socks5 proxy
+    
+    Args:
+        url (string): url to retreive 
+        headers (dict): mapping of headers for requests
+    """
     if headers:
         return session.head(url, timeout=timeout, headers=headers)
-    else:
-        return session.head(url, timeout=timeout)
 
+    return session.head(url, timeout=timeout)
