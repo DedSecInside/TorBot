2cd68a968fe31456f2d07d3f6f8f4dc5d3e4fb60
Akeem King <akeemtlking@gmail.com>
2019-05-25 10:06:03 -0400

    Start at home directory rather than root

--- modules/collect_data.py
+++ modules/collect_data.py
@@ -15,7 +15,7 @@ from .utils import find_file
 from threading import Lock
 
 
-dev_file = find_file("torbot_dev.env", "/")
+dev_file = find_file("torbot_dev.env", "/home")
 if not dev_file:
     raise FileNotFoundError
 load_dotenv(dotenv_path=dev_file)
