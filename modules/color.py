25972cd3992a0dc2ae4bd5995ecfbced8880b94a
Christian L Moreno <christianluciano.m@gmail.com>
2019-10-11 21:46:02 -0300

    pep8 code style in modules/color.py

--- modules/color.py
+++ modules/color.py
@@ -9,17 +9,18 @@ COLORS = {
     'cyan':     "\033[1;36m",
     'red':      "\033[1;31m",
     'magenta':  "\033[1;35m",
-    'black':      "\033[1;30m",
-    'darkwhite':  "\033[0;37m",
-    'darkyellow': "\033[0;33m",
-    'darkgreen':  "\033[0;32m",
-    'darkblue':   "\033[0;34m",
-    'darkcyan':   "\033[0;36m",
-    'darkred':    "\033[0;31m",
-    'darkmagenta':"\033[0;35m",
-    'darkblack':  "\033[0;30m",
-    'end':        "\033[0;0m"
+    'black':        "\033[1;30m",
+    'darkwhite':    "\033[0;37m",
+    'darkyellow':   "\033[0;33m",
+    'darkgreen':    "\033[0;32m",
+    'darkblue':     "\033[0;34m",
+    'darkcyan':     "\033[0;36m",
+    'darkred':      "\033[0;31m",
+    'darkmagenta':  "\033[0;35m",
+    'darkblack':    "\033[0;30m",
+    'end':          "\033[0;0m"
 }
+
 
 class color:
     """
