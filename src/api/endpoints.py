cb15db246ac59e7a7f4e619ddfbdb7bd7f10942f
Artifact <artifact@localhost.localdomain>
2020-08-01 23:03:34 +0530

    post method added

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append('..')
-
 import flask
 import argparse
 from flask import request,jsonify
@@ -10,14 +9,15 @@ app=flask.Flask(__name__)
 
 app.config["DEBUG"]=True
 
-@app.route('/<string:name>')
-def callTor(name: str):
-    url="http://danielas3rtn54uwmofdo3x2bsdifr47huasnmbgqzfrec5ubupvtpid.onion/"
-    args={'ip':'127.0.0.1','port':9050,'no_socks':False,'url':'http://danielas3rtn54uwmofdo3x2bsdifr47huasnmbgqzfrec5ubupvtpid.onion/','gather':'False','version':'False','update':'False',
+@app.route('/postApi',methods=['POST'])
+def callTor():
+    content = request.get_json(force = True)
+    url=content['url']
+    args={'ip':'127.0.0.1','port':9050,'no_socks':False,'url':url,'gather':'False','version':'False','update':'False',
           'quiet':'False','mail':'False','info':'False','save':'False','visualize':'False','depth':'False','download':'False'
     
     }
-    data1=tor.test(args)
-    return jsonify(data =str(data1)),200,{'Access-Control-Allow-Origin':'*'}
+    data1=test(args)
+    return jsonify(data =str(data1)),201,{'Access-Control-Allow-Origin':'*'}
     
 app.run()
