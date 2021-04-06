d3db38b44e70a49144932986b283aaa45d9db4f7
Anthony Suárez <neolight1010@gmail.com>
2021-04-06 15:18:38 -0500

    Add --paths flag to install.sh pyinstaller command.
    This change avoids certain relative import errors like:
        - "No module named modules"

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 mkdir -p tmp_build 
 mkdir -p tmp_dist
 
-pip install pyinstaller 
+pip install pyinstaller
 
 # Creates executable file and sends dependences to the recently created directories
-pyinstaller --onefile --workpath ./tmp_build --distpath ./tmp_dist src/torBot.py
+pyinstaller --onefile --workpath ./tmp_build --distpath ./tmp_dist --paths=src src/torBot.py
 
 # Puts the executable in the current directory
 mv tmp_dist/torBot . 
