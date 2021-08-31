3ce7cf0fef037d8d20497b730e47eda14c1fddc3
Akeem King <akeemtlking@gmail.com>
2021-08-31 14:57:24 -0400

    Linting

--- src/nlp/gather_data.py
+++ src/nlp/gather_data.py
@@ -2,20 +2,20 @@ import csv
 from os import write
 from pathlib import Path
 
-"""
-Writes the training data from the csv file to a directory based on the scikit-learn.datasets `load_files` specification.
-
-dataset source: https://www.kaggle.com/hetulmehta/website-classification
-
-e.g.
-container_folder/
-	category_1_folder/
-		file_1.txt file_2.txt file_3.txt ... file_42.txt
-	category_2_folder/
-		file_43.txt file_44.txt ...
-"""
 
 def write_data():
+	"""
+	Writes the training data from the csv file to a directory based on the scikit-learn.datasets `load_files` specification.
+
+	dataset source: https://www.kaggle.com/hetulmehta/website-classification
+
+	e.g.
+	container_folder/
+		category_1_folder/
+			file_1.txt file_2.txt file_3.txt ... file_42.txt
+		category_2_folder/
+			file_43.txt file_44.txt ...
+	"""
 	with open('website_classification.csv') as csvfile:
 		website_reader = csv.reader(csvfile, delimiter=',')
 		for row in website_reader:
@@ -28,5 +28,3 @@ def write_data():
 
 if __name__ == "__main__":
 	write_data()
-
-
