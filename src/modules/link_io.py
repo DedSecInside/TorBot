dc7a0cfa54163ece6b11f9ee90c27d6b45f253fe
Akeem King <akeemtlking@gmail.com>
2021-09-08 12:00:05 -0400

    Refactoring code and making use of more gotor endpoints

--- src/modules/link_io.py
+++ src/modules/link_io.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from .color import color
+from pprint import pprint
 
 
 def print_tor_ip_address():
@@ -14,38 +15,47 @@ def print_tor_ip_address():
     displays your IP address which we scape and display
     """
     print('Attempting to connect to https://check.torproject.org/')
-    response = requests.get('https://check.torproject.org/')
-    page = BeautifulSoup(response.text, 'html.parser')
-    ip_cont = page.find('strong')
-    ip_addr = ip_cont.renderContents()
-    ip_string = color(ip_addr.decode("utf-8"), 'yellow')
+    resp = requests.get(f'http://localhost:8081/ip')
+    ip_string = color(resp.text, 'yellow')
     print(f'Tor IP Address: {ip_string}')
 
-
-def display_children(node):
-    """
-    Static method to display status of child nodes
-    Args:
-        node (LinkNode): root of children to be displayed
-    """
-    children = node.get_children()
-    sucess_msg = color(f'Links Found - {len(children)}', 'green')
-    print(sucess_msg + '\n' + '---------------------------------')
-    for child in children:
-        display(child)
-
-
-def display(node):
+def print_node(node):
     """
     Prints the status of a link based on it's connection status
     Args:
         link (str): link to get status of
     """
     try:
-        title = node.get_name()
-        status = node.status
+        title = node['url']
+        if node['status_code'] >= 200 and node['status_code'] < 300:
+            status = color(f"{node['status_code']} {node['status']}", 'green')
+        elif node['status_code'] >= 300 and node['status_code'] < 400:
+            status = color(f"{node['status_code']} {node['status']}", 'yellow')
+        else:
+            status = color(f"{node['status_code']} {node['status']}", 'red')
     except Exception:
         title = "NOT FOUND"
         status = color('Unable to reach destination.', 'red')
-    status_msg = "%-30s %-20s %-70s" % (title, status, node.get_link())
+
+    status_msg = "%-60s %-20s" % (title, status)
     print(status_msg)
+
+def cascade(node, work):
+    work(node)
+    if node['children']:
+        for child in node['children']:
+            cascade(child, work)
+
+def print_tree(url, depth=1):
+    resp = requests.get(f'http://localhost:8081/children?link={url}&depth={depth}')
+    root = resp.json()
+    cascade(root, print_node)
+
+def print_json(url, depth=1):
+    resp = requests.get(f'http://localhost:8081/children?link={url}&depth={depth}')
+    root = resp.json()
+    pprint(root)
+    return root
+
+def print_emails(url, depth=1):
+    print(url, depth)
--- src/modules/utils.py
+++ src/modules/utils.py
@@ -4,105 +4,9 @@ Provides essential utilites for the rest of TorBot app.
 """
 import os
 
-from queue import Queue
-from threading import Thread
-
-import requests
-from requests.exceptions import HTTPError
-
 from dotenv import load_dotenv
 
-
-# ALGORITHM UTILITY FUNCTIONS
-
-
-def process_data(data_queue, data_stack, process, *args):
-    """
-    Processes tasks using by grabbing threads from queue
-
-    Args:
-       data_queue (queue.Queue): Contains tasks in FIFO data structure.
-       data_processor (function): Function to be executed on task and args.
-       data_args (tuple): Contains arguments for tasks.
-    """
-    while True:
-        try:
-            data = data_queue.get()
-            if isinstance(data, list):
-                for single_inst in data:
-                    if args:
-                        result = process(single_inst, args)
-                    else:
-                        result = process(single_inst)
-            elif args:
-                result = process(data, args)
-            else:
-                result = process(data)
-
-            if result:
-                data_stack.append(result)
-            data_queue.task_done()
-        except Exception as e:
-            pass
-
-
-def multi_thread(data, data_function, *args):
-    """
-    Start threads with function to process data and arguments then
-    process the data in FIFO order.
-
-    Args:
-        data (list): Lists of values that you'd like to operate on.
-        data_function (function): Function that you would like to use
-            for processsing.
-        args (tuple): Arguments for function.
-
-    Returns:
-        (list): List of processed data elements.
-    """
-    data_queue = Queue(len(data)*2)
-    ret_stack = list()
-    for _ in data:
-        data_args = (data_queue, ret_stack, data_function, *args)
-        thd = Thread(target=process_data, args=data_args)
-        thd.daemon = True
-        thd.start()
-
-    for obj in data:
-        data_queue.put(obj)
-
-    data_queue.join()
-    return ret_stack
-
-
-# Networking functions
-
-def get_url_status(url, headers=False):
-    """
-    Uses GET request to check if website exists.
-
-    *NOTE: May look into changing this to HEAD requests to improve perf
-
-    Args:
-        url (str): URL to be tested.
-
-    Return:
-        (response object): Return response object from connection
-            if successful.
-        (int): Return 0 if connection not successful.
-    """
-    try:
-        if headers:
-            resp = requests.get(url, headers=headers)
-        else:
-            resp = requests.get(url)
-        resp.raise_for_status()
-        return resp
-    except (ConnectionError, HTTPError):
-        return 0
-
-
-# File Functions 
+# File Functions
 
 def find_file(name, path):
     """Search for file within specific dir and any child dirs.
@@ -121,6 +25,15 @@ def find_file(name, path):
     return False
 
 def join_local_path(file_name=""):
+    """
+    Returns a path to the data directory with the given filename appended
+
+    Args:
+        file_name (str): filename to be joined
+
+    Returns:
+        str: local path to data directory
+    """
     if file_name == "":
         return
 
