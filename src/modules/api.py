f17cec9a333cc60f51968c6fa31733dd23a6d148
Akeem King <akeemtlking@gmail.com>
2021-09-14 05:38:47 -0400

    Update tree endpoint

--- src/modules/api.py
+++ src/modules/api.py
@@ -4,7 +4,7 @@ import requests
 class GoTor:
     @staticmethod
     def get_node(link, depth, address='localhost', port='8081'):
-        url = f'http://{address}:{port}/children?link={link}&depth={depth}'
+        url = f'http://{address}:{port}/tree?link={link}&depth={depth}'
         resp = requests.get(url)
         return resp.json()
 
