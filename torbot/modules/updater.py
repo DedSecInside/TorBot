75e437e199e7a32d4085cf579b3c5eacd2f4a939
PSNAppZ <thepsnarayanan@gmail.com>
2023-01-12 22:20:52 +0530

    Patch: Fixed flake8 issues

--- torbot/modules/updater.py
+++ torbot/modules/updater.py
@@ -1,11 +1,13 @@
 import subprocess
 
 def check_version():
+
     try:
         subprocess.run(["git", "branch"], check=True, capture_output=True)
     except subprocess.CalledProcessError:
         subprocess.run(["git", "init"], capture_output=True)
-        subprocess.run(["git", "remote", "add", "origin", "https://github.com/DedSecInside/TorBoT.git"], capture_output=True)
+        subprocess.run(["git", "remote", "add", "origin",
+         "https://github.com/DedSecInside/TorBoT.git"], capture_output=True)
 
     print("Checking for latest stable release")
     branch_out = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, text=True)
