db81913868a3c8cc90ee37dfbbcc21dd3e30c295
PSNAppZ <thepsnarayanan@gmail.com>
2023-01-12 22:12:20 +0530

    Fixed flake8

--- torbot/modules/updater.py
+++ torbot/modules/updater.py
@@ -2,11 +2,11 @@ import subprocess
 
 def check_version():
     try:
-        subprocess.run(["git", "branch"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["git", "branch"], check=True, capture_output=True)
     except subprocess.CalledProcessError:
-        subprocess.run(["git", "init"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.run(["git", "remote", "add", "origin", "https://github.com/DedSecInside/TorBoT.git"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        
+        subprocess.run(["git", "init"], capture_output=True)
+        subprocess.run(["git", "remote", "add", "origin", "https://github.com/DedSecInside/TorBoT.git"], capture_output=True)
+
     print("Checking for latest stable release")
     branch_out = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, text=True)
     branch = branch_out.stdout
