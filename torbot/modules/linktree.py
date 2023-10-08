d727061f73859219b9b5da7c1f8feeb7a4cc98b4
Akeem King <akeemtlking@gmail.com>
2023-10-08 11:29:32 -0400

    Remove LinkTree class and use treelib strcuture for hosting nodes

--- torbot/modules/linktree.py
+++ torbot/modules/linktree.py
@@ -2,87 +2,72 @@
 Module is used for analyzing link relationships
 """
 import os
+import re
+import httpx
+import validators
+import logging
 
-from treelib import Tree, exceptions
+from treelib import Tree, exceptions, Node
+from bs4 import BeautifulSoup
 
-from .api import get_node
-from .config import get_data_directory
-from .log import debug
+from .nlp.main import classify
 
-
-def formatNode(n):
-    return f"{n['url']} {n['status_code']} {n['status']}"
-
-
-def build_tree_recursive(t, n):
-
-    # this will only be ran on the root node since others will exist before being passed
-    parent_id = n["url"]
-    if not t.contains(parent_id):
-        debug(f"adding id {parent_id}")
-        t.create_node(formatNode(n), parent_id)
-
-    # if there are no children, there's nothing to process
-    children = n["children"]
-    if not children:
-        return
-
-    for child in children:
-        try:
-            child_id = child["url"]
-            debug(f"adding child_id {child_id} to parent_id {parent_id}")
-            t.create_node(formatNode(child), child_id, parent=parent_id)
-        except exceptions.DuplicatedNodeIdError:
-            debug(f"found a duplicate url {child_id}")
-            continue  # this node has already been processed somewhere else
-
-        build_tree_recursive(t, child)
+class Link(Node):
+    def __init__(self, title: str, url: str, status: int, classification: str, accuracy: float):
+        self.identifier = url
+        self.tag = title
+        self.status = status
+        self.classification = classification
+        self.accuracy = accuracy
 
 
-class LinkTree:
+def parse_links(html: str) -> list[str]:
     """
-    This is a class that represents a tree of links within TorBot. This can
-    be used to build a tree, examine the number of nodes, check if a node
-    exists within a tree, displaying the tree, and downloading the tree. It
-    will be expanded in the future to meet further needs.
+    Finds all anchor tags and parses the href attribute.
     """
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup.find_all('a')
+    return [tag['href'] for tag in tags if tag.has_attr('href') and validators.url(tag['href'])]
 
-    def __init__(self, root: str, depth: int):
-        self.__build_tree(root, depth)
 
-    def __build_tree(self, url: str, depth: int = 1):
-        """
-        Builds link tree by traversing through children nodes.
+def append_node(tree: Tree, id: str, parent_id: str | None) -> None:
+    """
+    Creates a node for a tree using the given ID which corresponds to a URL.
+    If the parent_id is None, this will be considered a root node.
+    """
+    resp = httpx.get(id)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    title = soup.title.text.strip() if soup.title is not None else id
+    try:
+        [classification, accuracy] = classify(resp.text)
+        data = Link(title, id, resp.status_code, classification, accuracy)
+        tree.create_node(title, identifier=id, parent=parent_id, data=data)
+    except exceptions.DuplicatedNodeIdError:
+        logging.debug(f"found a duplicate URL {id}")
 
-        Returns:
-            tree (ete3.Tree): Built tree.
-        """
-        debug(f"building tree for {url} at {depth} depth")
-        n = get_node(url, depth)
-        t = Tree()
-        build_tree_recursive(t, n)
-        self._tree = t
-        debug("tree built successfully")
 
-    def save(self, file_name: str):
-        """
-        Saves LinkTree to file with given file_name
-        Current file types supported are .txt
-        """
-        print(f"saving link tree as {file_name}")
-        data_directory = get_data_directory()
-        file_path = os.path.join(data_directory, file_name)
-        try:
-            self._tree.save2file(file_path)
-        except Exception as e:
-            print(f"failed to save link tree to {file_path}")
-            debug(e)
-            raise e
+def build_tree(tree: Tree, url: str, depth: int) -> None:
+    """
+    Builds a tree from the root to the given depth.
+    """
+    if depth > 0:
+        depth -= 1
+        resp = httpx.get(url)
+        children = parse_links(resp.text) 
+        for child in children:
+            append_node(tree, id=child, parent_id=url)
+            build_tree(tree, child, depth)
 
-        print(f"file saved successfully to {file_path}")
 
-    def show(self):
-        """
-        Displays image of LinkTree
-        """
-        self._tree.show()
+def save(tree: Tree, file_name: str) -> None:
+    """
+    Saves the tree to the current working directory under the given file name.
+    """
+    tree.save2file(os.path.join(os.getcwd(), file_name))
+
+
+def show(tree: Tree) -> None:
+    """
+    Prints the tree
+    """
+    tree.show()
