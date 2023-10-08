e06fa5000775e3ece5883c5db4479ab2508835af
Akeem King <akeemtlking@gmail.com>
2023-10-08 11:28:52 -0400

    Remove the use of gotor for building trees and retrieving IP address

--- torbot/modules/api.py
+++ torbot/modules/api.py
@@ -3,37 +3,58 @@ API Module
 
 Provides access to external services using API wrappers
 """
-import requests
+import httpx
+import logging
 
-from .log import debug
+from treelib import Tree
+from bs4 import BeautifulSoup, Tag
+
 from .config import host, port
+from .linktree import append_node, build_tree
 
 base_url: str = f'http://{host}:{port}'
 
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-def get_node(link: str, depth: int):
+def get_node(url: str, depth: int):
     """
     Returns the LinkTree for the given link at the specified depth.
     """
-    endpoint = f'/tree?link={link}&depth={depth}'
-    url = base_url + endpoint
-    debug(f'requesting {url}')
-    resp = requests.get(url)
-    data = resp.json()
-    debug(f'retrieved {data}')
-    return data
+    tree = Tree()
+    append_node(tree, id=url, parent_id=None)
+    build_tree(tree, url, depth)
+    return tree
 
 
-def get_ip():
+def get_ip() -> dict:
     """
     Returns the IP address of the current Tor client the service is using.
     """
-    endpoint = '/ip'
-    url = base_url + endpoint
-    debug(f'requesting {url}')
-    resp = requests.get(url)
-    debug(f'retrieved {resp.text}')
-    return resp.text
+    resp = httpx.get("https://check.torproject.org/")
+    soup = BeautifulSoup(resp.text, features='html.parser')
+
+    # Get the content of check tor project, this contains the header and body
+    content = soup.find("div", {"class": "content"})
+    if not content:
+        raise Exception("unable to find content to parse IP.")
+
+    # parse the header
+    header_tag = content.find("h1")
+    if not header_tag:
+        raise Exception("unable to find header")
+    if not isinstance(header_tag, Tag):
+        raise Exception("invalid header found")
+    header = header_tag.get_text().strip()
+
+    # parse the main content containing the IP address
+    body_tag = content.find("p")
+    if not body_tag:
+        raise Exception("unable to find body")
+    if not isinstance(body_tag, Tag):
+        raise Exception("invalid body found")
+    body = body_tag.get_text().strip()
+
+    return {"header": header, "body": body}
 
 
 def get_emails(link: str):
@@ -42,10 +63,8 @@ def get_emails(link: str):
     """
     endpoint = f'/emails?link={link}'
     url = base_url + endpoint
-    debug(f'requesting {url}')
-    resp = requests.get(url)
+    resp = httpx.get(url)
     data = resp.json()
-    debug(f'retrieved {data}')
     return data
 
 
@@ -55,10 +74,8 @@ def get_phone(link: str):
     """
     endpoint = f'/phone?link={link}'
     url = base_url + endpoint
-    debug(f'requesting {url}')
-    resp = requests.get(url)
+    resp = httpx.get(url)
     data = resp.json()
-    debug(f'retrieved {data}')
     return data
 
 
@@ -69,6 +86,6 @@ def get_web_content(link: str):
     endpoint = f'/content?link={link}'
     url = base_url + endpoint
     debug(f'requesting {url}')
-    resp = requests.get(url)
+    resp = httpx.get(url)
     debug(f'retrieved {resp.text}')
     return resp.text
