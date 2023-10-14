a2ed34488d2533f7a85e812bde86351294813a27
Akeem King <akeemtlking@gmail.com>
2023-10-14 14:05:01 -0400

    Adding test for parse_links

--- torbot/modules/tests/test_linktree.py
+++ torbot/modules/tests/test_linktree.py
@@ -1,4 +1,6 @@
-from ..linktree import parse_hostname
+from yattag import Doc
+
+from ..linktree import parse_hostname, parse_links
 
 
 def test_parse_hostname() -> None:
@@ -7,3 +9,20 @@ def test_parse_hostname() -> None:
 
     http_test_url = 'https://www.test.com'
     assert parse_hostname(http_test_url) == 'www.test.com'
+
+
+def test_parse_links() -> None:
+    doc, tag, text = Doc().tagtext()
+    with tag('html'):
+        with tag('a', href='https://example.com'):
+            pass
+        with tag('a', href='http://test.com'):
+            pass
+        with tag('a', href='https://example-test.com'):
+            pass
+        with tag('a', href='invalid link'):
+            pass
+
+    links = parse_links(doc.getvalue())
+    assert len(links) == 3
+    assert links == ['https://example.com', 'http://test.com', 'https://example-test.com']
