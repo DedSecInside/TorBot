4572af981addbbc1139f0d8fb3e3d2b11f2d0a0a
Akeem King <akeemtlking@gmail.com>
2023-10-26 07:46:08 -0400

    Use CLI flags instead of environment variables for SOCKS5 proxy configuration and black formatter changes

--- torbot/modules/config.py
+++ torbot/modules/config.py
@@ -7,23 +7,20 @@ from unipath import Path
 source_file = getsourcefile(lambda: 0)
 config_file_path = None
 if isinstance(source_file, str):
-    config_file_path = (os.path.abspath(source_file))
+    config_file_path = os.path.abspath(source_file)
 
 if not config_file_path:
-    raise Exception('Unable to load environment.')
+    raise Exception("Unable to load environment.")
 
 modules_directory = Path(config_file_path).parent
 torbot_directory = modules_directory.parent
 project_root_directory = torbot_directory.parent
-dotenv_path = os.path.join(project_root_directory, '.env')
+dotenv_path = os.path.join(project_root_directory, ".env")
 load_dotenv(dotenv_path=dotenv_path, verbose=True)
-
-socks5_host = os.getenv('SOCKS5_HOST')
-socks5_port = os.getenv('SOCKS5_PORT')
 
 
 def get_data_directory():
-    data_directory = os.getenv('TORBOT_DATA_DIR')
+    data_directory = os.getenv("TORBOT_DATA_DIR")
     # if a path is not set, write data to the config directory
     if not data_directory:
         data_directory = project_root_directory
